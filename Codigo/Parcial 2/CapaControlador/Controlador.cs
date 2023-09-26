fb3a70f7cbb1a17908351dff21cbb099ec35f502
carlos enrique <cguzmanc10@miumg.edu.gt>
2023-09-26 09:14:01 -0600

    boton listo

---
+++ Codigo/Parcial 2/CapaControlador/Controlador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo;
+using System.Data.Odbc;
+using System.Data;
+
+namespace CapaControlador
+{
+    public class Controlador
+    {
+        Sentencias sn = new Sentencias();
+
+       
+     
+
+        
+    
+
+
+       
+    
+
+   
+
+        public List<string> ObtenerDatosM(string columna1, string columna2, string columna3, string columna4, string columna5, string columna6, string tabla)
+        {
+            return sn.ObtenerDatosM(columna1,columna2, columna3, columna4, columna5, columna6, tabla);
+        }
+
+
+      
+
+
+
+       
+    }
+}
---
+++ Codigo/Parcial 2/CapaModelo/Sentencias.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using System.Security.Cryptography;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CapaModelo
+{
+    public class Sentencias
+    {
+        Conexion con = new Conexion();
+
+
+       
+
+
+
+       
+
+        
+
+
+     
+
+        public List<string> ObtenerDatosM(string columna1,string columna2, string columna3, string columna4, string columna5, string columna6, string tabla)
+        {
+            List<string> datos = new List<string>();
+            try
+            {
+
+                string consulta = $"SELECT {columna1},{columna2},{columna3},{columna4},{columna5},{columna6} FROM {tabla}";
+
+                OdbcCommand command = new OdbcCommand(consulta, con.conexion());
+                OdbcDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string carnet = reader[columna1].ToString();
+                    string nombre = reader[columna2].ToString();
+                    string direccion = reader[columna3].ToString();
+                    string telefono = reader[columna4].ToString();
+                    string email = reader[columna5].ToString();
+                    string estatus = reader[columna6].ToString();
+
+                    string cadena = carnet + " - " +nombre + " - "+ direccion+" - "+telefono+" - "+email+" - "+estatus;
+                    
+                    datos.Add(cadena);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return datos;
+        }
+
+      
+    }
+}
